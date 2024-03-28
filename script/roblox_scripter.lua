994029ee9b81a2b9b730aed3c966e445a9b1c3ae
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2024-03-28 11:43:50 +0300

    Update roblox_scripter.lua

--- script/roblox_scripter.lua
+++ script/roblox_scripter.lua
@@ -1,2 +1,10 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("RoBloX_Scripter", "BloodTheme")
+local Window = Library.CreateLib("RoBloX_Scripter", "DarkTheme")
+
+local Tab = Window:NewTab("script")
+local Section = Tab:NewSection("Local Script ")
+
+Section:NewButton("ButtonText", "ButtonInfo", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/yeerma/1/main/bot'))(
+end)
+
