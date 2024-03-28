3e2ab430c51f6e3e7b22d76df6db6efa2a42dd7b
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2024-03-28 11:46:07 +0300

    Update roblox_scripter.lua

--- script/roblox_scripter.lua
+++ script/roblox_scripter.lua
@@ -4,7 +4,33 @@ local Window = Library.CreateLib("RoBloX_Scripter", "DarkTheme")
 local Tab = Window:NewTab("script")
 local Section = Tab:NewSection("Local Script ")
 
-Section:NewButton("ButtonText", "ButtonInfo", function()
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/yeerma/1/main/bot'))(
+Section:NewButton("Sworld Bot", "Auto kick", function()
+  
 end)
 
+  Section:NewButton("ButtonText", "ButtonInfo", function()
+  --// Setting \\--
+local range = 15
+
+--// Variable \\--
+local player = game:GetService("Players").LocalPlayer
+
+--// Script \\--
+game:GetService("RunService").RenderStepped:Connect(function()
+    local p = game.Players:GetPlayers()
+    for i = 2, #p do local v = p[i].Character
+        if v and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 and v:FindFirstChild("HumanoidRootPart") and player:DistanceFromCharacter(v.HumanoidRootPart.Position) <= range then
+            local tool = player.Character and player.Character:FindFirstChildOfClass("Tool")
+            if tool and tool:FindFirstChild("Handle") then
+                tool:Activate()
+                for i,v in next, v:GetChildren() do
+                    if v:IsA("BasePart") then
+                        firetouchinterest(tool.Handle,v,0)
+                        firetouchinterest(tool.Handle,v,1)
+                    end
+                end
+            end
+        end
+    end
+end)
+end)
